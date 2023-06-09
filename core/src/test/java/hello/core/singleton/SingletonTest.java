da559793720391be75ef017ea8e18f289fe428ec
WonSeok-dd <cluejws@gmail.com>
2023-06-09 16:50:21 +0900

    feat(Section-05 싱글톤 컨테이너): (싱글톤 컨테이너)

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -20,5 +22,19 @@ public class SingletonTest {
 
         //Then
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴 적용한 스프링 컨테이너")
+    void springContainer(){
+        //given
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //when(조회) 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //Then
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
