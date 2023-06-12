456d921ae90b4e3606ce40668c03ccfdcb596a4e
WonSeok-dd <cluejws@gmail.com>
2023-06-13 01:43:08 +0900

    feat(Section-09 스프링 빈 스코프): 스프링 빈 스코프 - 싱글톤 스코프, 프로토타입 스코프 함께 사용 문제

--- TIL/TIL9.md
+++ TIL/TIL9.md
@@ -62,3 +62,84 @@
   - 스프링 컨테이너가 이후는 관리하지 않음
     - 프로토타입 빈을 관리할 책임은 **_클라이언트_**
     - `@PreDestroy` 같은 종료 메소드 호출X
+
+<br/>
+
+### ✅ 스프링 빈 스코프 - 싱글톤 스코프, 프로토타입 스코프 함께 사용 시 문제점
+- **_클라이언트 -> 프로토타입 빈_** 직접 요청
+  - ![img9_4.png](file/img9_4.png)
+  - ![img9_5.png](file/img9_5.png)
+    - 클라이언트 A가 프로토타입 빈을 스프링 컨테이너에 요청
+    - 스프링 컨테이너 새로운 스프링 빈 반환
+    - 클라이언트 A가 프로토타입 빈 `addCount()` 호출 및 `count 필드 + 1 -> 1` 세팅
+      <br><br>
+    - 클라이언트 B가 프로토타입 빈을 스프링 컨테이너에 요청
+    - 스프링 컨테이너 새로운 스프링 빈 반환
+    - 클라이언트 B가 프로토타입 빈 `addCount()` 호출 및 `count 필드 + 1 -> 1` 세팅
+
+
+- **_클라이언트 -> 싱글톤 빈 -> 프로토타입 빈_** 요청
+  - ![img9_6.png](file/img9_6.png)
+  - ![img9_7.png](file/img9_7.png)
+  - ![img9_8.png](file/img9_8.png)
+    - `clientBean`은 싱글톤이므로 **_의존관계 자동 주입_** 을 통해, 프로토타입 빈 요청
+      - 주입 시점에 스프링 컨테이너에 프로토타입 빈 요청
+      - 스프링 컨테이너는 프로토타입 빈 생성해 `clientBean`에 반환
+      - `clientBean`은 프로토타입 빈 내부 필드에 보관
+      <br><br>
+    - 클라이언트 A가 싱글톤 빈(`clientBean`)을 스프링 컨테이너에 요청
+    - 스프링 컨테이너 해당 스프링 빈 반환
+    - 클라이언트 A가 싱글톤 빈(`clientBean`)의 `addCount()` 호출 및 `count 필드 + 1 -> 1` 세팅
+      <br><br>
+    - 클라이언트 B가 싱글톤 빈(`clientBean`)을 스프링 컨테이너에 요청
+    - 스프링 컨테이너 해당 스프링 빈 반환
+    - 클라이언트 A가 싱글톤 빈(`clientBean`)의 `addCount()` 호출 및 `count 필드 + 1 -> 2` 세팅
+
+  - `clientBean`이 내부에 가지고 있는 프로토타입 빈은 이미 과거에 주입이 끝난 빈<br>
+    사용 할 때마다 새로 생성되는 것이 **_아님_**
+  - ```java
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean){
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            this.prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+    ```
+
+
+- 해결) **_클라이언트 -> 싱글톤 빈 -> 프로토타입 빈_** 요청
+  - **_프로토타입 빈_** 을 **_주입 시점에만 새로 생성하는 것_** 이 아니라, <br>
+    **_사용할 때 마다 생성하는 것_** 이 사용 이유
+  - ```java
+    @Scope("singleton")
+    static class ClientBean {
+
+        @Autowired
+        ApplicationContext applicationContext;
+
+        public int logic() {
+            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+    ```
+    - **_의존관계 주입(DI = Dependency Injection)_** -> **X**
+    - **_의존관계 조회(DL = Dependency Lookup)_** -> **O**
+    - 스프링 컨테이너에 종속적
+    - 단위 테스트 어렵
+    
+
+- **_여러 빈에서 같은 프로토타입 빈_** 을 주입 받으면, **_주입 받는 시점_** 에 **_각각 새로운 프로토타입 빈_** 이 생성
+  - **_clientA, clientB_** 가 각각 의존관계 주입을 받으면, **_각각 다른 인스턴스의 프로토타입
+    빈_** 을 주입
+    - clientA prototypeBean@x01
+    - clientB prototypeBean@x02
---
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeScopeTest.java
@@ -0,0 +1,97 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonWithPrototypeScopeTest {
+
+    @Test
+    void prototypeFind() {
+        //given
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        //when
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+
+        //then
+        Assertions.assertThat(prototypeBean1.getCount()).isEqualTo(1);
+        Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
+
+        // 스프링 컨테이너가 빈 등록, 의존관계 주입, 초기화 후 관리 X
+        // 소멸전 콜백 -> destroy() 실행 X
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+        ac.close();
+    }
+
+    @Test
+    void singletonClientUsePrototype(){
+        //given
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        //when
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+
+        //then
+        Assertions.assertThat(count1).isEqualTo(1);
+        Assertions.assertThat(count2).isEqualTo(2);
+
+        // 스프링 컨테이너가 빈 등록, 의존관계 주입, 초기화 후 관리 X
+        // 소멸전 콜백 -> destroy() 실행 X
+        // prototypeBean1.destroy(); -> 하지만 언급x
+        // prototypeBean2.destroy(); -> 하지만 언급x
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean){
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            this.prototypeBean.addCount();
+            return this.prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            this.count++;
+        }
+
+        public int getCount() {
+            return this.count;
+        }
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init = " + this);
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy = " + this);
+        }
+    }
+}
